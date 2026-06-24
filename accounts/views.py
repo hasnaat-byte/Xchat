dfc393a271a7b663b7cbb140fa5b585bcab6352f
hasnaat-byte <hasnaatmalik29@gmail.com>
2026-06-24 19:29:55 +0530

    v0.11

--- accounts/views.py
+++ accounts/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib.auth import login
 from django.contrib.auth import authenticate
+from django.contrib.auth import logout
 
 def register(request):
     if request.method == "POST":
@@ -37,3 +38,7 @@ def user_login(request):
             return redirect("users")
         
     return render(request, "login.html")
+
+def user_logout(request):
+    logout(request)
+    return redirect("login")
