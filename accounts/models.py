bc5fc89b5c04f7450ed29e40979baf4229c50098
hasnaat-byte <hasnaatmalik29@gmail.com>
2026-06-27 23:59:32 +0530

    v0.17

--- accounts/models.py
+++ accounts/models.py
@@ -16,5 +16,11 @@ class Profile(models.Model):
         auto_now_add=True
     )
 
+    profile_picture = models.ImageField(
+        upload_to="profile_pics/",
+        blank=True,
+        null=True
+    )
+
     def __str__(self):
         return self.user.username
